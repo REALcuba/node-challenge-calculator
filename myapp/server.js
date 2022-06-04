c1b8772898d8db12f4b67b9f724db50f3afa0185
REALcuba <angelskcuba@gmail.com>
2022-06-04 16:28:26 +0200

    first query done

--- myapp/server.js
+++ myapp/server.js
@@ -1,23 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('http://localhost:3000/add?value1=10&value2=2', (req, res) => {
-    res.send('Hello Express')
+app.get("/add", (req, res) => {
+  const value1 = Number(req.query.value1);
+  const value2 = Number(req.query.value2);
+
+  let add = value1 + value2;
+  res.send(`${add}`);
 });
 
+app.get("/substract", (req, res) => {
+  const value1 = Number(req.query.value1);
+  const value2 = Number(req.query.value2);
 
-app.get('http://localhost:3000/substract?value1=10&value2=2', (req, res) => {
-    res.send('Hello Express')
+  let substract = value1 - value2;
+  res.send(`${substract}`);
 });
 
+app.get("/multiply", (res, req) => {
+  const value1 = Number(req.query.value1);
+  const value2 = Number(req.query.value2);
 
-app.get('http://localhost:3000/multiply?value1=10&value2=2', (req, res) => {
-    res.send('Hello Express')
+  let div = value1 * value2;
+  res.send(`${div}`);
 });
 
+app.get("/divide", (req, res) => {
+  const value1 = Number(req.query.value1);
+  const value2 = Number(req.query.value2);
 
-app.get('http://localhost:3000/divide?value1=10&value2=2', (req, res) => {
-    res.send('Hello Express')
+  let multiply = value1 / value2;
+  res.send(`${multiply}`);
 });
 
-app.listen(3000, () => console.log("Server is up and running"))
+// app.get("multiply/:value1=10/:value2=2", (req, res) => {
+//   let div = Number(req.query.value1 * Number(req.query.value2));
+//   res.send(`${div}`);
+// });
+
+// app.get("/add/:value1=10/:value2=2", (req, res) => {
+//   let add = Number(req.query.value1 + Number(req.query.value2));
+//   res.send(`${add}`);
+// });
+// app.get("/substract/:value1=10/:value2=2", (req, res) => {
+//   let sub = Number(req.query.value1 - Number(req.query.value2));
+//   res.send(`${sub}`);
+// });
+// app.get("/divide/:value1=10/:value2=2", (req, res) => {
+//   let div = Number(req.query.value1 / Number(req.query.value2));
+//   res.send(`${div}`);
+// });
+
+app.listen(3000, () => console.log("Server is up and running"));
